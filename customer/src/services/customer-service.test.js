1fdf4c300cc70bd03fc86e318694769fcab18942
Doki Sriram <111217410+dokisriram@users.noreply.github.com>
2024-03-09 20:42:42 +0530

    changes on customer-service file (#4)

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -2,7 +2,7 @@
 describe("CustomerService", () => {
     // which function we are testing for sign in
     describe("SignIn", () => {
-        // which scneario we are testing
+        // which scneario we are testing for signin
         test("validate user inputs", () => {
             
         })
